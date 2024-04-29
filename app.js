5d01af8ab5af6a22d74700a0eb27682550c076ec
galihwicakshm <galihwicaksonomukti2000@gmail.com>
2024-04-29 20:11:38 +0700

    remove view

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var userRouter = require('./routes/user');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
